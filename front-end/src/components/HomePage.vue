2fd4fbaf7534e3280f1b6be1e08de9a198796340
giuseppevignanello <peppe.vignanello@gmail.com>
2023-10-19 14:27:00 +0200

    add filter

--- front-end/src/components/HomePage.vue
+++ front-end/src/components/HomePage.vue
@@ -4,7 +4,18 @@ import { ref, onMounted } from 'vue';
 import axios from 'axios';
 const apiUrl = "http://localhost:8080/api/v1.0";
 const photos = ref(null);
+const search = ref(false);
+const searchTerm = ref('');
+const searchResults = ref(null);
 
+
+function searchPhoto() {
+  axios.get(`${apiUrl}/filter/${searchTerm.value}`)
+    .then((response) => {
+      search.value = true;
+      searchResults.value = response.data;
+    })
+}
 
 function getAll() {
   axios.get(apiUrl)
@@ -24,7 +35,34 @@ onMounted(() => {
 
 <template>
   <div>
-    <ul>
+    <div>
+      <form @submit.prevent="searchPhoto">
+        <label for="search">Search a Photo</label>
+        <input type="text" v-model="searchTerm">
+        <button type="submit">Search</button>
+      </form>
+    </div>
+    <div v-if="search">
+      <ul v-if="searchResults.length > 0">
+        <li v-for="photo in searchResults">
+          <div v-if="photo.visible">
+            <h2>{{ photo.title }}</h2>
+            <p>{{ photo.description }}</p>
+            <img :src="photo.url" :alt="photo.tilte">
+            <h4>Categories</h4>
+            <ul>
+              <li v-for="category in photo.categories">
+                {{ category.name }}
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+      <div v-else>
+        Nessun risultato
+      </div>
+    </div>
+    <ul v-if="!search">
       <li v-for="photo in photos">
         <div v-if="photo.visible">
           <h2>{{ photo.title }}</h2>
