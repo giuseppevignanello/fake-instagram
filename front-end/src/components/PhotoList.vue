bfd7b1f1e7c09e0373e756b59190f7c5ad930d60
giuseppevignanello <peppe.vignanello@gmail.com>
2023-10-20 10:03:12 +0200

    fix search functionality

--- front-end/src/components/PhotoList.vue
+++ front-end/src/components/PhotoList.vue
@@ -6,16 +6,16 @@ import sendMessage from "./SendMessage.vue";
 const apiUrl = "http://localhost:8080/api/v1.0";
 const photos = ref(null);
 const showCategories = ref(false);
+const showSearchBar = ref(false);
 const search = ref(false);
 const searchTerm = ref('');
-const searchResults = ref(null);
 
 
 function searchPhoto() {
     axios.get(`${apiUrl}/filter/${searchTerm.value}`)
         .then((response) => {
             search.value = true;
-            searchResults.value = response.data;
+            photos.value = response.data;
         })
 }
 function getAll() {
@@ -35,7 +35,40 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="container mt-3">
+    <div class="container">
+        <p class="text-center fs-6"> Click on a story to open the search function</p>
+        <div class="stories d-flex gap-2 justify-content-between my-3 mx-2">
+            <div @click="showSearchBar = !showSearchBar" class="story">
+                <img src="https://www.wilsoncenter.org/sites/default/files/media/images/person/james-person-1.jpg" alt="">
+            </div>
+            <div @click="showSearchBar = !showSearchBar" class="story">
+                <img src="https://images.healthshots.com/healthshots/en/uploads/2020/12/08182549/positive-person.jpg"
+                    alt="">
+
+            </div>
+            <div @click="showSearchBar = !showSearchBar" class="story">
+                <img src="https://www.verywellmind.com/thmb/brQcgvNiCkT4O7WhkL8YIDssDSo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-4660327211-56b5fae93df78c0b13571d1e.jpg"
+                    alt="">
+
+            </div>
+            <div @click="showSearchBar = !showSearchBar" class="story">
+                <img src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F352059889%2F28138890009%2F1%2Foriginal.20220911-152029?w=600&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C77%2C680%2C340&s=30a1d3171906fdc8e8c567a434b03542"
+                    alt="">
+            </div>
+            <div @click="showSearchBar = !showSearchBar" class="story">
+                <img src="https://contenthub-static.grammarly.com/blog/wp-content/uploads/2016/11/Point-of-View.jpg" alt="">
+            </div>
+            <div @click="showSearchBar = !showSearchBar" class="story">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1O8H1chF14jAFC7flzk3XOakXSmGUC6QEhg&usqp=CAU"
+                    alt="">
+            </div>
+            <div @click="showSearchBar = !showSearchBar" class="story">
+                <img src="https://health.clevelandclinic.org/wp-content/uploads/sites/3/2023/01/Highly-Sensitive-Person-1360038205-770x533-1.jpg"
+                    alt="">
+            </div>
+        </div>
+    </div>
+    <div :class="{ 'd-none': !showSearchBar }" class="container mt-3">
         <div class="row">
             <div class="col-md-6">
                 <form @submit.prevent="searchPhoto">
@@ -47,84 +80,55 @@ onMounted(() => {
             </div>
         </div>
     </div>
-    <div class="container  mt-3">
-        <div v-if="search">
-            <ul v-if="searchResults.length > 0" class="list-unstyled">
-                <li v-for="photo in searchResults">
-                    <div v-if="photo.visible" class="d-flex flex-column">
+    <div class="container mt-3">
+        <ul class="list-unstyled row row-cols-12 align-items-center">
+            <li v-for="photo in photos">
+                <div v-if="photo.visible" class="d-flex flex-column justify-content-center">
+                    <div class="photo_header d-flex align-items-center gap-2">
+                        <div class="photo_header_img">
+                            <img src="https://picsum.photos/200/300" alt="">
+                        </div>
+                        <div class="d-flex gap-2 align-items-center">
+                            <h6 class="photo_title">{{ photo.title }}</h6>
+                            <p class="title_days mt-2"> - 2 g</p>
+                        </div>
+                    </div>
+
+                    <div class="mt-3">
+                        <div class="photo_image">
+                            <img :src="photo.url" :alt="photo.tilte">
+                        </div>
+                    </div>
+                    <div class="icons mt-2 d-flex gap-2">
                         <div>
-                            <h6>{{ photo.title }}</h6>
+                            <i class="fa-regular fa-heart fa-xl"></i>
                         </div>
-                        <div class="d-flex justify-content-center ">
-                            <div class="photo_image">
-                                <img :src="photo.url" :alt="photo.tilte">
-                            </div>
+                        <div data-bs-toggle="modal" data-bs-target="#sendMessageModal">
+                            <i class="fa-regular fa-message fa-xl"></i>
                         </div>
-
-                        <p>{{ photo.description }}</p>
-                        <h6>Categories</h6>
-                        <ul>
-                            <li v-for="category in photo.categories">
-                                {{ category.name }}
-                            </li>
-                        </ul>
+                        <div data-bs-toggle="modal" data-bs-target="#sendMessageModal">
+                            <i class="fa-regular fa-paper-plane"></i>
+                        </div>
                     </div>
-                </li>
-            </ul>
-            <div v-else>
-                Nessun risultato
-            </div>
-        </div>
-        <div class="container mt-3">
-            <ul v-if="!search" class="list-unstyled row row-cols-12 align-items-center">
-                <li v-for="photo in photos">
-                    <div v-if="photo.visible" class="d-flex flex-column justify-content-center">
-                        <div class="photo_header d-flex align-items-center gap-2">
-                            <div class="photo_header_img">
-                                <img src="https://picsum.photos/200/300" alt="">
-                            </div>
-                            <div class="d-flex gap-2 align-items-center">
-                                <h6 class="photo_title">{{ photo.title }}</h6>
-                                <p class="title_days mt-2"> - 2 g</p>
-                            </div>
+                    <div class="liked_by mt-2 d-flex gap-1">
+                        <div class="liked_img">
+                            <img src="https://picsum.photos/200/300" alt="">
                         </div>
-
-                        <div class="mt-3">
-                            <div class="photo_image">
-                                <img :src="photo.url" :alt="photo.tilte">
-                            </div>
-                        </div>
-                        <div class="icons mt-2 d-flex gap-2">
-                            <div>
-                                <i class="fa-regular fa-heart fa-xl"></i>
-                            </div>
-                            <div data-bs-toggle="modal" data-bs-target="#sendMessageModal">
-                                <i class="fa-regular fa-message fa-xl"></i>
-                            </div>
-                            <div data-bs-toggle="modal" data-bs-target="#sendMessageModal">
-                                <i class="fa-regular fa-paper-plane"></i>
-                            </div>
-                        </div>
-                        <div class="liked_by mt-2 d-flex gap-1">
-                            <div class="liked_img">
-                                <img src="https://picsum.photos/200/300" alt="">
-                            </div>
-                            <p>Liked by <strong> Vignanello </strong> and <strong> others </strong></p>
-                        </div>
-                        <span class="mb-1 photo_description"> <strong> description </strong> {{ photo.description }}</span>
-                        <span @click="showCategories = !showCategories" class="show_categories">Mostra tutte le
-                            categorie</span>
-                        <ul class="list-unstyled" :class="{ 'd-none': !showCategories }">
-                            <li v-for="category in photo.categories">
-                                {{ category.name }}
-                            </li>
-                        </ul>
+                        <p>Liked by <strong> Vignanello </strong> and <strong> others </strong></p>
                     </div>
-                </li>
-            </ul>
-        </div>
-        <sendMessage></sendMessage>
+                    <span class="mb-1 photo_description"> <strong> description </strong> {{ photo.description }}</span>
+                    <span @click="showCategories = !showCategories" class="show_categories">Mostra tutte le
+                        categorie</span>
+                    <ul class="list-unstyled" :class="{ 'd-none': !showCategories }">
+                        <li v-for="category in photo.categories">
+                            {{ category.name }}
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
     </div>
+    <sendMessage></sendMessage>
 </template>
 
 
--- src/main/java/org/java/app/SpringIlMioFotoalbumApplication.java
+++ src/main/java/org/java/app/SpringIlMioFotoalbumApplication.java
@@ -15,71 +15,69 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SpringIlMioFotoalbumApplication implements CommandLineRunner{
+public class SpringIlMioFotoalbumApplication implements CommandLineRunner {
 
 	@Autowired
 	private PhotoService photoService;
-	
-	@Autowired 
+
+	@Autowired
 	private CategoryService categoryService;
-	
+
 	@Autowired
 	private RoleService roleService;
 	@Autowired
 	private UserService userService;
-	
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIlMioFotoalbumApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Role admin = new Role("ADMIN");
 		Role user = new Role("USER");
-		
+
 		roleService.save(admin);
 		roleService.save(user);
-		
+
 		final String pwsAdmin = new BCryptPasswordEncoder().encode("password");
 		final String pwsUser = new BCryptPasswordEncoder().encode("password");
-		
+
 		User myAdmin = new User("admin", pwsAdmin, admin);
 		User myUser = new User("user", pwsUser, user);
 		User myUser2 = new User("user2", pwsUser, user);
-		
+
 		userService.save(myAdmin);
 		userService.save(myUser);
 		userService.save(myUser2);
-		
+
 		Category landscape = new Category("landscape");
 		Category portrait = new Category("portrait");
 		Category macro = new Category("macro");
 		Category wildlife = new Category("wildlife");
 		Category street = new Category("street");
-		Category food= new Category("food");
-		
-		
+		Category food = new Category("food");
+
 		categoryService.save(landscape);
 		categoryService.save(portrait);
 		categoryService.save(macro);
 		categoryService.save(wildlife);
 		categoryService.save(street);
 		categoryService.save(food);
-		
-		Photo photo1 = new Photo("Photo1", "La prima foto", "https://picsum.photos/200/300", true, false, myUser, landscape, portrait);
-		Photo photo2 = new Photo("Photo2", "La seconda foto", "https://picsum.photos/200/300", true, false, myUser, wildlife);
-		Photo photo3 = new Photo("Photo3", "La terza foto", "https://picsum.photos/200/300", true, false, myUser2, street, food);
-		
-		photoService.save(photo1); 
-		photoService.save(photo2); 
-		photoService.save(photo3);
-		
-		
-		
-		
-		
+
+		Photo bear = new Photo("bear", "A bear in its natural habitat",
+				"https://cdn.naturettl.com/wp-content/uploads/2020/08/04123339/creative-ways-to-improve-your-wildlife-photography-800x534.jpg?p=20603",
+				true, false, myUser, landscape, wildlife);
+		Photo red_hiding_hood = new Photo("Red Hiding Hood", "A little girl dressed as a red riding hood",
+				"https://images.unsplash.com/photo-1624711517157-25991163e537?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9ydHJhaXQlMjBwaG90b2dyYXBoeXxlbnwwfHwwfHx8MA%3D%3D",
+				true, false, myUser, portrait);
+		Photo thai_food = new Photo("Street Food in Thailand", "La terza foto", "https://images.pexels.com/photos/920570/pexels-photo-920570.jpeg?cs=srgb&dl=pexels-min-an-920570.jpg&fm=jpg", true, false, myUser2,
+				street, food);
+
+		photoService.save(bear);
+		photoService.save(red_hiding_hood);
+		photoService.save(thai_food);
 
 	}
 
